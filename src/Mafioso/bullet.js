98c566b2c0a299d88187a9297d26ef2e0379f8af
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2021-11-23 12:31:11 +0100

    la bala hace daño al player y ya se mueve con fisicas

--- src/Mafioso/bullet.js
+++ src/Mafioso/bullet.js
@@ -4,34 +4,36 @@ export default class Bullet extends Phaser.GameObjects.Sprite {
         super(scene, x, y, 'bullet', player);
 
         this.scene.add.existing(this);
-        //this.scene.physics.add.existing(this, true);
+        this.scene.physics.add.existing(this);
+
+        this.body.setAllowGravity(false);
 
         this.scale = 0.1;
         this.damage = 1;
         
         this.angle = (Phaser.Math.Angle.Between(this.x, this.y, player.x, player.y)); 
         this.jugador = player;
-                
-        /*this.scene.physics.add.collider(player, this,(o1,o2)=> {
-            onCollision(o1,o2);
-        });*/
 
+        this.speedBullet = 300;
     }
 
     preUpdate(){
         super.preUpdate();
         
         if(!this.scene.isPaused()){
-            this.x += 10*Math.cos(this.angle);
-            this.y += 10*Math.sin(this.angle);
-            /*this.body.setVelocityX((this.scene.physics.moveTo(this, this.jugador.x, this.jugador.y, 10)*180)/3.141516);
-            this.body.setVelocityY((this.scene.physics.moveTo(this, this.jugador.x, this.jugador.y, 10)*180)/3.141516);*/
+            /*this.x += 10*Math.cos(this.angle);
+            this.y += 10*Math.sin(this.angle);*/
+            this.body.setVelocityX(this.speedBullet*Math.cos(this.angle));
+            this.body.setVelocityY(this.speedBullet*Math.sin(this.angle));
         }
 
-        //this.scene.physics.moveTo(this, this.jugador.x, this.jugador.y, 10);
+        this.scene.physics.add.collider(this.jugador, this,(o1,o2)=> {
+            this.handleCollision();
+            this.destroy();
+        });
     }
 
     handleCollision(){
-        this.player.loseLife(this.damage);
+        this.jugador.loseLife(this.damage);
     }
 }
