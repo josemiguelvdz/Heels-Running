8d7af4dc275a59a66f1c1521b3a3249defac6c58
josemiguelvdz <josemiguelv2002@gmail.com>
2021-12-03 11:55:40 +0100

    Tilemap started

---
+++ mapaPVLI/tilesets/road.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="road" tilewidth="840" tileheight="600" tilecount="1" columns="1">
+ <image source="D:/Servidor/PVLI/assets/backgrounds/road.png" width="840" height="600"/>
+</tileset>
