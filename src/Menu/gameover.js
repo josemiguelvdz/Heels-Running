7cb5e14742fb021653cfc910f83adb22df8df4d8
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2021-12-03 17:36:33 +0100

    gameover centrado

--- src/Menu/gameover.js
+++ src/Menu/gameover.js
@@ -12,12 +12,12 @@ export default class GameOver extends Phaser.Scene {
 
         this.gameOverBackGround = this.add.image(this.scale.width*0.5, this.scale.height*0.5, 'gameoverbackground').setScale(2, 2).setScrollFactor(0);
           
-        this.spriteGameOver = this.add.image(500, 200, 'gameover').setScale(0.8, 0.8);
+        this.spriteGameOver = this.add.image(this.scale.width*0.5, 200, 'gameover').setScale(0.8, 0.8);
 
-        this.playButton = this.add.image(500, 400, 'playButton').setInteractive();
+        this.playButton = this.add.image(this.scale.width*0.5, 400, 'playButton').setInteractive();
         this.playButton.on('pointerdown', () => {this.scene.start('level')});
 
-        this.exitButton = this.add.image(500, 500, 'exitButton').setInteractive();
+        this.exitButton = this.add.image(this.scale.width*0.5, 500, 'exitButton').setInteractive();
         this.exitButton.on('pointerdown', () => {this.scene.start('menu')})
     }
 }
