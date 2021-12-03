39d8339c48ac56c7bd67bebc95ad05c685b13c52
Daniel <druiz07@ucm.es>
2021-12-03 12:01:22 +0100

    Ajustadas las rutas de los tiles

--- mapaPVLI/tilesets/sky.tsx
+++ mapaPVLI/tilesets/sky.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="sky" tilewidth="840" tileheight="600" tilecount="1" columns="1">
- <image source="D:/Servidor/PVLI/assets/backgrounds/sky.png" width="840" height="600"/>
+ <image source="../../assets/backgrounds/sky.png" width="840" height="600"/>
 </tileset>
